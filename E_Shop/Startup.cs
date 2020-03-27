d7cc0fa0431d1835f9edd67732085600de72cee2
mylland <alexandru1998c@mail.ru>
2020-03-27 15:59:37 +0200

    Add project files.

---
+++ E_Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Shop.Startup))]
+namespace E_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
